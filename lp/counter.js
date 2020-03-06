2d87b85399a95663525fb473bfb61fd7f235d84d
potenc1 <asdasddsaivanov@yandex.ru>
2020-03-06 15:56:49 +0400

    Initial commit

---
+++ lp/counter.js
@@ -0,0 +1,48 @@
+var yandex = {
+    "v1": 51633617,
+    "v2": 51633671,
+    "v3": 51633737,
+    "v4": 51633785,
+    "v5": 51633824,
+    "v6": 51633845,
+    "v7": 53626567,
+    "v8": 53630020,
+    "v9": 53630083,
+    "v10": 53630110,
+    "v11": 54157774,
+};
+
+function $_GET(key) {
+    var s = window.location.search;
+    s = s.match(new RegExp(key + '=([^&=]+)'));
+    return s ? s[1] : '';
+}
+
+var site = $_GET('version').replace('de', '').replace('39', '')
+
+if (!site) {
+    var site = document.location.pathname
+        .replace(/\/+/g, '')
+        .replace('index.html', '')
+        .replace('index2.html', '')
+        .replace('prelp', '')
+        .replace('de', '')
+        .replace('39', '')
+        .replace('confirm.html', '');
+
+}
+(function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+    };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(yandex[site], "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+});
